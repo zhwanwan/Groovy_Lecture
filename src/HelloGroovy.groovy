4e0cf20ab1bdd41202b5da4e73e185023e20739f
zhwanwan <334271937@qq.com>
2019-07-24 22:27:24 +0800

    init

---
+++ src/HelloGroovy.groovy
@@ -0,0 +1,13 @@
+/*class HelloGroovy {
+
+//    public static void main(String[] args) {
+//        System.out.println("Hello Groovy!")
+//    }
+
+
+    static void main(args) {
+        System.out.println("Hello Groovy!")
+    }
+}*/
+
+println "Hello Groovy!"
---
+++ src/com/lec/grov01/Variable.groovy
@@ -0,0 +1,39 @@
+package com.lec.grov01
+
+/**
+ * groovy中的变量：
+ * 基本类型/对象类型都会转成对象类型
+ *
+ */
+
+//强类型定义方式，跟Java中类似
+int x = 10;
+println(x.class) //class java.lang.Integer
+
+double y = 3.14
+println y.class  //class java.lang.Double
+
+Float z = 5.5
+println(z.class) //class java.lang.Float
+
+println("---------------------------")
+
+//定义弱类型变量(用def关键字)，所谓弱类型就是可以动态转换成其他类型
+def x_1 = 11
+def y_1 = 3.1415
+def name = "Android"
+println(x_1.class)
+println(y_1.class)
+println(name.class)
+x_1 = "Groovy"
+println x_1.class
+println("---------------------------")
+
+
+//String类型
+def str = 'a single string' //用单引号定义一个string
+def str1 = 'Tom\'s\' cat'   //转义字符'
+println(str.class)
+println str1
+def tripleName = '''three single string''' //三引号字符串
+println(tripleName)
